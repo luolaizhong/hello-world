086c30c8c44207627269cc818e4289be6ec60dd7
luolaizhong <luolaizhong8844@163.com>
2019-06-30 10:09:00 +0800

    二叉查找

---
+++ myproject/src/main/java/core/IntDemo.java
@@ -0,0 +1,11 @@
+package core;
+
+public class IntDemo {
+    public static void main(String[] args) {
+        System.out.println(Math.round(2.4));
+        System.out.println(Math.round(-2.4));
+
+        System.out.println(Math.floor(2.4));
+        System.out.println(Math.floor(-2.4));
+    }
+}

47f7fcbed66c23348c1806ffd30727d11cab38f2
luolaizhong <luolaizhong8844@163.com>
2019-06-30 19:06:36 +0800

    修改java加载器demo

--- myproject/src/main/java/core/ClassLoaderDemo.java
+++ myproject/src/main/java/core/ClassLoaderDemo.java
@@ -2,17 +2,17 @@ package core;
 
 public class ClassLoaderDemo {
     public static void main(String[] args) {
-//        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
         try {
-//            Class<HashDemo> clazz = (Class<HashDemo>) classLoader.loadClass("core.HashDemo");
-            Class<HashDemo> clazz = HashDemo.class;
+            Class<HashDemo> clazz = (Class<HashDemo>) classLoader.loadClass("core.HashDemo");
+//            Class<HashDemo> clazz = HashDemo.class;
             System.out.println(clazz);
             HashDemo hd = clazz.newInstance();
             hd.setName("jack");
             hd.setAge(10);
             System.out.println(hd);
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {

d0ed344db5ba46dbc65ea05e359126ee02513632
luolaizhong <luolaizhong8844@163.com>
2019-06-30 15:47:57 +0800

    线程按顺序执行：join的使用

--- myproject/src/main/java/core/IntDemo.java
+++ myproject/src/main/java/core/IntDemo.java
@@ -2,9 +2,15 @@ package core;
 
 public class IntDemo {
     public static void main(String[] args) {
+        //向右取整
+        System.out.println(5 / 2);
+        System.out.println(-5 / 2);
+
+        //四舍五入，返回long类型
         System.out.println(Math.round(2.4));
         System.out.println(Math.round(-2.4));
 
+        //向做取整，返回double类型
         System.out.println(Math.floor(2.4));
         System.out.println(Math.floor(-2.4));
     }

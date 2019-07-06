492f16352784d51a1042f0b427000ac6278030c6
luolaizhong <luolaizhong8844@163.com>
2019-07-06 15:14:40 +0800

    同步阻塞bio

---
+++ CoreJava/src/main/java/core/io/bio/BIOClient.java
@@ -0,0 +1,37 @@
+package core.io.bio;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class BIOClient {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+        for (int i = 0; i < 10; i++) {
+            final int n = i + 1;
+            executorService.execute(() -> {
+                OutputStream out = null;
+                try {
+                    Socket socket = new Socket("localhost", 50050);
+                    out = socket.getOutputStream();
+                    String str = n + " say hello world";
+                    System.out.println(str);
+                    out.write(str.getBytes("utf-8"));
+                    out.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        out.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        executorService.shutdown();
+    }
+}
---
+++ CoreJava/src/main/java/core/io/bio/BIOServer.java
@@ -0,0 +1,25 @@
+package core.io.bio;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class BIOServer {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(50050);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                InputStream in = socket.getInputStream();
+                byte[] buff = new byte[1024];
+                int len;
+                while ((len = in.read(buff)) != -1) {
+                    System.out.println(new String(buff, 0, len));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

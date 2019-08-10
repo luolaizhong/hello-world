fe35a35e34d027348a9420bfdedc45d288d9420f
luolaizhong <luolaizhong8844@163.com>
2019-08-10 14:48:51 +0800

    update round

--- corejava/src/main/java/com/llz/core/IntDemo.java
+++ corejava/src/main/java/com/llz/core/IntDemo.java
@@ -6,47 +6,47 @@ package com.llz.core;
 public class IntDemo {
     public static void main(String[] args) {
         //取整数部分
-        System.out.println(5 / 2);
-        System.out.println(-5 / 2);
-        System.out.println(5 / 4);
-        System.out.println(-5 / 4);
+        System.out.println(5 / 2);  // 2
+        System.out.println(-5 / 2); //-2
+        System.out.println(5 / 4);  // 1
+        System.out.println(-5 / 4); //-1
 
         System.out.println("====================");
 
         //向左取整，返回double类型
-        System.out.println(Math.floor(2.4));
-        System.out.println(Math.floor(-2.4));
-        System.out.println(Math.floor(2.5));
-        System.out.println(Math.floor(-2.5));
+        System.out.println(Math.floor(2.4));    // 2.0
+        System.out.println(Math.floor(-2.4));   //-3.0
+        System.out.println(Math.floor(2.5));    // 2.0
+        System.out.println(Math.floor(-2.5));   //-3.0
 
         System.out.println("====================");
 
         //相当于+0.5后向左取整，返回long类型
-        System.out.println(Math.round(2.4));
-        System.out.println(Math.round(-2.4));
-        System.out.println(Math.round(2.5));
-        System.out.println(Math.round(-2.5));
+        System.out.println(Math.round(2.4));    // 2
+        System.out.println(Math.round(-2.4));   //-2
+        System.out.println(Math.round(2.5));    // 3
+        System.out.println(Math.round(-2.5));   //-2
 
         System.out.println("====================");
 
-        // 而真正的四舍五入是不考虑符号进行四舍五入，上面任何一种都不是四舍五入
-        System.out.println(realRound(2.4));
-        System.out.println(realRound(-2.4));
-        System.out.println(realRound(2.5));
-        System.out.println(realRound(-2.5));
+        // 上面任何一种都不是四舍五入，真正的四舍五入是绝对值进行四舍五入，最后加上符号
+        System.out.println(realRound(2.4));     // 2
+        System.out.println(realRound(-2.4));    //-2
+        System.out.println(realRound(2.5));     // 3
+        System.out.println(realRound(-2.5));    //-3
     }
 
     /**
      * 真正的四舍五入
      *
-     * @param f 小数
+     * @param d 小数
      * @return 整数
      */
-    private static long realRound(double f) {
-        if (f >= 0) {
-            return Math.round(f);
+    private static long realRound(double d) {
+        if (d >= 0) {
+            return Math.round(d);
         } else {
-            return -Math.round(-f);
+            return -Math.round(-d);
         }
     }
 }
